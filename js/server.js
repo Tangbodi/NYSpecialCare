ff0627441fa6d660cfb464f4b52c4095e12e734a
tangbodi <tangbodi@gmail.com>
2024-11-30 23:32:38 -0500

    modify
    modified url of API

--- js/server.js
+++ js/server.js
@@ -56,7 +56,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
